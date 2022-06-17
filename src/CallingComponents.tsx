83729b4f3be3c5c2fe914b182fcf87fe6f021544
alkwa-msft <79329532+alkwa-msft@users.noreply.github.com>
2022-06-17 14:31:43 +0000

    creating 200x200 tiles

--- src/CallingComponents.tsx
+++ src/CallingComponents.tsx
@@ -1,59 +1,48 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 import {
-    CameraButton,
-    ControlBar,
-    EndCallButton,
-    GridLayout,
-    MicrophoneButton,
-    DevicesButton,
-    ScreenShareButton,
-    VideoTile,
-    usePropsFor,
-    VideoGallery
-  } from '@azure/communication-react';
-  
-  import { IContextualMenuProps, mergeStyles, Stack } from '@fluentui/react';
-  import React, { useState } from 'react';
+  CameraButton,
+  ControlBar,
+  EndCallButton,
+  GridLayout,
+  MicrophoneButton,
+  DevicesButton,
+  ScreenShareButton,
+  VideoTile,
+  usePropsFor,
+  VideoGallery
+} from '@azure/communication-react';
+
+import { IContextualMenuProps, mergeStyles, Stack } from '@fluentui/react';
+import React, { useState } from 'react';
 import { ConnectedLocalVideoTile } from './ConnectedLocalVideoTile';
 import { ConnectedRemoteVideoTile } from './ConnectedRemoteVideoTile';
-  
-  export const CallingComponents = (): JSX.Element => {
-    const exampleOptionsMenuProps: IContextualMenuProps = {
-      items: [
-        {
-          key: '1',
-          name: 'Choose Camera',
-          iconProps: { iconName: 'LocationCircle' },
-          onClick: () => alert('Choose Camera Menu Item Clicked!')
-        }
-      ]
-    };
-  
-    const controlBarStyle = {
-      root: {
-        justifyContent: 'center'
-      }
-    };
-    const videoGalleryProps = usePropsFor(VideoGallery);
-    const {cameras, checked, disabled, onToggleCamera, selectedCamera} = usePropsFor(CameraButton);
-    return (
-      <Stack className={mergeStyles({ height: '100%' })}>
-        {/* GridLayout Component relies on the parent's height and width, so it's required to set the height and width on its parent. */}
-        <div style={{ height: '30rem', width: '30rem', border: '1px solid' }}>
-            <ConnectedLocalVideoTile localParticipant={videoGalleryProps.localParticipant}/>
-            {
-                videoGalleryProps.remoteParticipants.map(remoteParticipant => <ConnectedRemoteVideoTile remoteParticipant={remoteParticipant} />)
-            }
-          {/* <GridLayout>
-            <VideoTile displayName={'Michael'} />
-          </GridLayout> */}
-        </div>
-  
-        {/* Control Bar with default set up */}
-        <ControlBar styles={controlBarStyle}>
-          <CameraButton checked={checked} disabled={disabled} onToggleCamera={onToggleCamera} selectedCamera={selectedCamera} cameras={cameras} />
-        </ControlBar>
-      </Stack>
-    );
+
+export const CallingComponents = (): JSX.Element => {
+  const controlBarStyle = {
+    root: {
+      justifyContent: 'center'
+    }
   };
+
+  const videoGalleryProps = usePropsFor(VideoGallery);
+  const {cameras, checked, disabled, onToggleCamera, selectedCamera} = usePropsFor(CameraButton);
+  const microphoneButtonProps = usePropsFor(MicrophoneButton);
+  return (
+    <Stack className={mergeStyles({ height: '100%' })}>
+      {/* GridLayout Component relies on the parent's height and width, so it's required to set the height and width on its parent. */}
+      <div>
+        <div style={{ width: '200px', height: '200px'}}> <ConnectedLocalVideoTile localParticipant={videoGalleryProps.localParticipant}/></div>
+            {
+                videoGalleryProps.remoteParticipants.map(remoteParticipant => <div style={{ width: '200px', height: '200px'}}> <ConnectedRemoteVideoTile remoteParticipant={remoteParticipant} /></div>)
+            }
+        </div>
+
+      {/* Control Bar with default set up */}
+      <ControlBar styles={controlBarStyle}>
+        <CameraButton checked={checked} disabled={disabled} onToggleCamera={onToggleCamera} selectedCamera={selectedCamera} cameras={cameras} />
+        <MicrophoneButton {...microphoneButtonProps} />
+      </ControlBar>
+    </Stack>
+  );
+};
